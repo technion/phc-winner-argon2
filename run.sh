0db352b3f7b535bc18aea64c7f3fbbdcefc6a542
Technion <technion@lolware.net>
2016-04-21 08:59:23 +0000

    Shrink test suite for performance. Fully build without threads.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./tis-interpreter.sh --cc "-I/code/include -lpthread" /code/src/test.c /code/src/argon2.c /code/src/encoding.c /code/src/core.c /code/src/blake2/blake2b.c /code/src/thread.c /code/src/pthread.c
+./tis-interpreter.sh --cc "-I/code/include -lpthread" /code/src/test.c /code/src/argon2.c /code/src/encoding.c /code/src/core.c /code/src/blake2/blake2b.c /code/src/thread.c /code/src/ref.c
